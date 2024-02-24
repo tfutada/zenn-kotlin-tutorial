289fc0cdee31b50e4058139975cb33920b2765b5
tfutada <tafutada@gmail.com>
2024-02-24 13:00:24 +0900

    Add CommandLineRunner to MytutorialApplication
    A CommandLineRunner bean has been added to MytutorialApplication.kt. This change allows code to be executed after the Spring Application has started, which is currently set to print "Hello, World!".

--- src/main/kotlin/jp/tf/mytutorial/MytutorialApplication.kt
+++ src/main/kotlin/jp/tf/mytutorial/MytutorialApplication.kt
@@ -1,10 +1,19 @@
 package jp.tf.mytutorial
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class MytutorialApplication
+class MytutorialApplication {
+	@Bean
+	fun commandLineRunner(): CommandLineRunner {
+		return CommandLineRunner {
+			println("Hello, World!")
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<MytutorialApplication>(*args)
