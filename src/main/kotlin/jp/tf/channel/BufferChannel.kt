a47b6badc77700adfde89f14bfa6e332eb742ba3
tfutada <tafutada@gmail.com>
2024-02-24 14:41:34 +0900

    Add BufferChannel to jp.tf.channel package
    A new file, BufferChannel.kt, was created in the jp.tf.channel package. This file includes code to demonstrate the use of a buffered channel, including sending elements, handling buffer suspension, launching coroutines, and cancelling them.

---
+++ src/main/kotlin/jp/tf/channel/BufferChannel.kt
@@ -0,0 +1,20 @@
+package jp.tf.channel
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    val channel = Channel<Int>(4) // create buffered channel
+    val sender = launch { // launch sender coroutine
+        repeat(10) {
+            println("Sending $it") // print before sending each element
+            channel.send(it) // will suspend when buffer is full
+        }
+    }
+
+    // don't receive anything... just wait....
+    delay(1000)
+    sender.cancel() // cancel sender coroutine
+}
